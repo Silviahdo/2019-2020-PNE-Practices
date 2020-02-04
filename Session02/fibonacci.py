720e00977cf3383a0fcf1e7af214df2d6bd92986
shernan <silviah108@hotmail.es>
2020-02-04 16:29:39 +0100

    fibonacci series

--- Session02/fibonacci.py
+++ Session02/fibonacci.py
@@ -1,7 +1,12 @@
 # Create a file with the fibonacci numbers
-
 count = 0
+n1 = 0
+n2 = 1
 for i in range(0, 11):
-    count = i + (i - 1)
+    print(n1, end=' ')
+    n = n1 + n2
+    n1 = n2
+    n2 = n
+    count += i
 
-print(count)
+
