d3a9a7e2ed92987a8d1deb8acf8bc623ff65b9c3
shernan <silviah108@hotmail.es>
2020-02-04 16:08:11 +0100

    XD

---
+++ Session02/fibonacci.py
@@ -0,0 +1,7 @@
+# Create a file with the fibonacci numbers
+
+count = 0
+for i in range(0, 11):
+    count = i + (i - 1)
+
+print(count)
--- Session02/sumN.py
+++ Session02/sumN.py
@@ -1,16 +1,11 @@
 # -- 1 + 1 + 2 + 3 + ... + 20
 # -- 1 + ......... + 100
+def sumn(n):
+    res = 0
+    for i in range(1, n+1):
+        res += i
+    return res
 
-res20 = 0
-
-for i in range(1, 21):
-    res20 += i
-
-res100 = 0
-
-for i in range(1, 101):
-    res100 += i
-
-print("sum from 1-20 is:", res20)
-print("sum from 1-100:", res100)
+print("sum of 1-20: ", sumn(20))
+print("sum from 1-100: ", sumn(100))
 
