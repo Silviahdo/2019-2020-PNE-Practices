f5a96df23bd635c45e1be00e326cc305b5747ac6
shernan <silviah108@hotmail.es>
2020-02-04 15:40:52 +0100

    no

---
+++ Session01/sum20.py
@@ -0,0 +1,12 @@
+#Session 1. Exercise 3
+#Calculate the sum of the first 20 integer numbers
+# 1 + 2 + 3 +...+ 20
+# Example working!
+
+#Store the result
+res = 0
+
+for i in range(1, 21):
+    res += i
+
+print("Total sum: res")
