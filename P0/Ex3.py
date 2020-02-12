d7bbed5973ee9a08dad7eb6372af35e2c06277da
shernan <silviah108@hotmail.es>
2020-02-12 18:46:08 +0100

    Session 5- ex3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -3,4 +3,9 @@ from Seq0 import *
 FOLDER = "../Session04/"
 filename = ["U5.txt", "ADA.txt", "FRAT1.txt", "RNU6_269P.txt"]
 
-list_genes
+list_genes = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+
+for element in filename:
+    seq = seq_read_fasta(FOLDER + element)
+    print(seq_len(seq))
+
